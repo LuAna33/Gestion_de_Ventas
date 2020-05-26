b35112abd88b739d268c956e6296a656c797c828
Luisa Analia Garin <luy22@live.com.ar>
2020-05-26 19:33:22 -0300

    N-ventas (#13)
    * Prueba de funcionamiento
    
    * Branch provisorio para modificaciones de Venta
    
    * Multiples Ventas

--- Venta.java
+++ Venta.java
@@ -1,93 +1,91 @@
 
 public class Venta { 
         int codVenta;
-        int preciounit;
         int cantidad;
         int cantidadMinima;
         int descuentoPorcentaje;
+        String continuarComprando;
         Cliente cliente;
         Articulo articulo;
-            
-    public Venta (int codVenta, int preciounit, int cantidad,int cantidadMinima,int descuentoPorcentaje){
+       
+    public Venta (int codVenta,int cantidad,int cantidadMinima,int descuentoPorcentaje){
         this.codVenta = codVenta;
-        this.preciounit = preciounit;
         this.cantidad = cantidad;
         this.cantidadMinima = cantidadMinima;
         this.descuentoPorcentaje = descuentoPorcentaje;
     }
-         
-    public int calcularPrecioCantidad(){
-        return preciounit * cantidad;        
-    }
+                       
+      int calcularPrecioCantidad(){
+           return cantidad * this.getArticulo().getPreciounit();       
+      }
     
-    public int calcularDescuentoCantidad (){
-        return calcularPrecioCantidad() * descuentoPorcentaje / 100;
-    }
+      int calcularDescuentoCantidad (){
+            return calcularPrecioCantidad() * descuentoPorcentaje / 100;
+      }
     
-    public int calcularPrecioFinal (){
-        int precioCantidad = calcularPrecioCantidad();
-        int descuentoCantidad = calcularDescuentoCantidad(); 
+      int calcularPrecioFinal (){
+           int precioCantidad = calcularPrecioCantidad();
+           int descuentoCantidad = calcularDescuentoCantidad(); 
            
-        if (cantidad >= cantidadMinima){
-            return precioCantidad - descuentoCantidad;
-        }else{
-            return precioCantidad;
-        }       
-    }
-      
-    Cliente getCliente(){
-        return cliente;
-    }
-    void setCliente(Cliente cliente){
-        this.cliente = cliente;
-    }
-    Articulo getArticulo(){
-        return articulo;
-    }
-    void setArticulo (Articulo articulo){
-        this.articulo = articulo;
-    }
-    int getCodVenta(){
-        return codVenta;
-    }
-    void setCodVenta(int codVenta ){
-        this.codVenta = codVenta;
-    }
-    int getCantidad(){
-        return cantidad;
-    }
+           if (cantidad >= cantidadMinima){
+              return precioCantidad - descuentoCantidad;
+           }
+           else{
+              return precioCantidad;
+           }       
+      }
     
-    void setCantidad (int cantidad){
-        this.cantidad = cantidad;  
-    }
     
-    int getPreciounit(){
-        return preciounit;
-    }
+      Cliente getCliente(){
+            return cliente;
+      }
     
-    void setPreciounit(int preciounit){
-        this.preciounit = preciounit;
-    }
-                  
-    int getCantidadMinima(){
+      void setCliente(Cliente cliente){
+           this.cliente = cliente;
+      }
+   
+      Articulo getArticulo(){
+            return articulo;
+      }
+   
+      void setArticulo (Articulo articulo){
+            this.articulo = articulo;
+      }
+    
+      int getCodVenta(){
+            return codVenta;
+      }
+         
+      void setCodVenta(int codVenta ){
+            this.codVenta = codVenta;
+      }
+         
+      int getCantidad(){
+            return cantidad;
+      }
+    
+      void setCantidad (int cantidad){
+            this.cantidad = cantidad;  
+      }
+           
+      int getCantidadMinima(){
         return cantidadMinima;
-    }
-    void setCantidadMinima(int cantidadMinima){
+      }
+      
+      void setCantidadMinima(int cantidadMinima){
         this.cantidadMinima = cantidadMinima;
-        
-    }
+      }
     
-    int getDescuentoPorcentaje (){
-        return descuentoPorcentaje;
-    }
+      int getDescuentoPorcentaje (){
+         return descuentoPorcentaje;
+      }
     
-    void setDescuentoPorcentaje (int descuentoPorcentaje){
+      void setDescuentoPorcentaje (int descuentoPorcentaje){
         this.descuentoPorcentaje = descuentoPorcentaje;
-              
-    }
-        
+      }
+                  
     public void imprimirVenta (){
-                        
+        
             System.out.println ("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
             System.out.println ("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<DETALLE DE LA VENTA>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
             System.out.println ("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
@@ -96,13 +94,15 @@ public class Venta {
             System.out.println ("///La cantidad minima para acceder al descuento es..." + getCantidadMinima() + "///");
             System.out.println ("///El porcentaje de descuento por cantidad es..." + getDescuentoPorcentaje() + "///");
             System.out.println ("///El monto final es ///$" + calcularPrecioFinal()+ "///");
-            System.out.println ("¡¡¡Gracias por su compra!!!");
-            
+            System.out.println ("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+            System.out.println ("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
             getCliente().imprimirCliente();
             getArticulo().imprimirArticulo();
+   }
+   
+}
 
-    }
-}        
+  
     
     
 
